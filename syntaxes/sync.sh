c0c656bbc921862bea6a6702b4033a4f44d9ebf2
chmn <chmn.oh@gmail.com>
2021-10-02 19:12:01 +0900

    include directive

--- syntaxes/sync.sh
+++ syntaxes/sync.sh
@@ -1,4 +1,4 @@
-ext_dir=$(echo $USERPROFILE|sed 's;\\;/;g')"/.vscode/extensions/chmn.klang-0.0.1"
+ext_dir=$(echo $USERPROFILE|sed 's;\\;/;g')"/.vscode/extensions/chmn-oh.k-lang-0.0.1"
 cp ../package.json $ext_dir/.
 cp ../language-configuration.json $ext_dir/.
 cp klang.tmLanguage.json $ext_dir/syntaxes/.
